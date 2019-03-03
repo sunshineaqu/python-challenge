6fc4026cb57403f8d61dcacb7ed0ee161115ae30
Lisa <lisalqin@gmail.com>
2019-03-03 15:02:15 -0600

    print to file and terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,18 +18,27 @@ with open(csvpath, newline="") as csvfile: #open the csvfile
         if row[2] not in candList:
             candList.append(row[2]) #get the candidate names
     
-    print("Election Results")
-    print("-------------------------------")
-    print(f"Total Votes: {total_votes}")
-    print("-------------------------------")
+    f = open("output.txt", "w") #new file for write
+    f.write("Election Results\n")
+    f.write("-------------------------------\n")
+    f.write(f"Total Votes: {total_votes}\n")
+    f.write("-------------------------------\n")
     
     for x in range(len(candList)):
         counts = voteList.count(candList[x]) #count the candidate name in votelist
         countList.append(counts) #add the number to countlist
         percentage = round((counts / total_votes *100),2)
-        print(f"{candList[x]}: {percentage}% ({counts})")
+        f.write(f"{candList[x]}: {percentage}% ({counts})\n")
     
-    winner_index = countList.index(max(countList))
-    print("-------------------------------")
-    print(f"Winner: {voteList[winner_index]}")
-    print("-------------------------------")
+    winner_index = countList.index(max(countList)) # get the winner name using index
+
+    f.write("-------------------------------\n")
+    f.write(f"Winner: {voteList[winner_index]}\n")
+    f.write("-------------------------------\n")
+    f.close()
+
+    f = open("output.txt", "r") #open the file as r
+    file_contents = f.read() 
+    print(file_contents)  # print to terminal
+    f.close()
+    
