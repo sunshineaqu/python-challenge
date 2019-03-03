51281053750126002eb6e1da63e4296096ced556
Lisa <lisalqin@gmail.com>
2019-03-03 13:54:07 -0600

    add counts for candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,36 +7,22 @@ with open(csvpath, newline="") as csvfile: #open the csvfile
     csvreader = csv.reader(csvfile, delimiter=",") #read the file
     csv_header = next(csvfile) #skip the header
 
-    votes = 0
-    counts = 0
+    total_votes = 0
     candList = [] #make a list for candidates
     voteList = [] #make a list to hold all the vote result
     
     for row in csvreader: #loop through entire rows
         voteList.append(row[2])
-        votes += 1 #calculate the votes
+        total_votes += 1 #calculate the votes
         if row[2] not in candList:
             candList.append(row[2]) #get the candidate names
     
-    
-    for row in csvreader:
-        for x in candlist:
-            if candList[x] == row[2]:
-                count += 1
-        print(f"{x} has {count}")
-
-exit(0)
-
-    '''
-    #how to define the variable to 
-    for x in range(votes-1)
-
-    NoteList = []
-    candList[0]
-    ..
-    candList[len(candList)-1]
-
-    
     print("Election Results")
     print("-------------------------------")
-    print(f"Total Votes: {votes}")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------------")
+    
+    for x in range(len(candList)):
+        counts = voteList.count(candList[x])
+        percentage = round((counts / total_votes *100),2)
+        print(f"{candList[x]}: {percentage}% ({counts})")
